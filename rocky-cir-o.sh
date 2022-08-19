4f6c7ecce032be4c23dc2be6a3d02911345916d6
julia <bigowo.digital@outlook.com>
2022-08-20 02:55:18 +0800

    Create rocky-cir-o.sh

---
+++ rocky-cir-o.sh
@@ -0,0 +1,11 @@
+# cir-o.io repo
+
+OS="Centos_8"
+VERSION="1.24"
+
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+
+sudo dnf update --refresh -y
+sudo dnf install cri-o
